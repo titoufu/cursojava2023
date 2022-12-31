5b8777edc7506f4c52de9f3e241b2cb91a89b52e
jribeiro <jribeiro@ufu.br>
2022-12-31 12:07:50 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -0,0 +1,77 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String Name;
+	private String email;
+	private String phone;
+	private String password;
+	public User() {
+		 
+	}
+	public User(Integer id, String name, String email, String phone, String password) {
+		super();
+		this.id = id;
+		Name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", Name=" + Name + ", email=" + email + ", phone=" + phone + ", password=" + password
+				+ "]";
+	}
+	
+
+}
