0cbd5ea23dbd8982ba925f63dd65515aebbed236
jribeiro <jribeiro@ufu.br>
2023-01-01 12:32:11 -0300

    JPA repository, dependency injection, database seeding - Tito

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -22,7 +22,8 @@ public class TestConfig implements CommandLineRunner {
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u3 = new User(null, "Tito Ribeiro", "tito@gmail.com", "987654321", "654321");
 
-		userRepository.saveAll(Arrays.asList(u1, u2));
+		userRepository.saveAll(Arrays.asList(u1, u2,u3));
 	}
 }
